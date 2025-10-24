7540a647773b8a2a07780d146346a1d66a66fce2
najongjine <najongjin3@hotmail.com>
2025-10-24 11:25:10 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./component/Header";
 import Home from "./page/Home";
 import Footer from "./component/Footer";
+import MemoDetail from "./page/MemoDetail";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/memo_detail" element={<MemoDetail />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/page/MemoDetail.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import * as gtypes from "../types/global_types";
+
+export default function MemoDetail() {
+  const [searchParams] = useSearchParams();
+  const memoId = Number(searchParams?.get("id") ?? 0);
+  const navigate = useNavigate();
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const [memo, setMemo] = useState<gtypes.Memotype>({
+    content: "",
+    createdDt: "",
+    updatedDt: "",
+    id: 0,
+    title: "",
+  });
+
+  useEffect(() => {
+    getMemo();
+  }, []);
+
+  function onMemoUpsert(id = 0) {
+    navigate(`/memo_upsert?id=${id}`);
+  }
+
+  async function getMemo() {
+    try {
+      const fetchOption = {
+        method: "GET",
+        headers: {
+          Authorization: "",
+        },
+      };
+      let result: any = await fetch(
+        `${API_BASE_URL}/api/board/get_memo_by_id?id=${memoId}`,
+        fetchOption
+      );
+      result = await result.json();
+      if (!result?.success) {
+        alert(`메모 데이터 가져오기 실패. ${result?.msg}`);
+        return;
+      }
+      setMemo({
+        content: result?.data?.content ?? "",
+        createdDt: result?.data?.createdDt ?? "",
+        updatedDt: result?.data?.updatedDt ?? "",
+        id: result?.data?.id ?? 0,
+        title: result?.data?.title ?? "",
+      });
+    } catch (error: any) {
+      console.log(`서버 에러! ${error?.message ?? ""}`);
+    }
+  }
+
+  return (
+    <div className="content-margin-padding">
+      <div>상세내용</div>
+
+      <div>제목: {memo?.title}</div>
+      <div>내용: {memo?.content}</div>
+
+      <button
+        onClick={(event) => {
+          onMemoUpsert(memo?.id ?? 0);
+        }}
+      >
+        수정
+      </button>
+
+      <button
+        onClick={async () => {
+          try {
+            const formData = new FormData();
+            formData.append("id", String(memo?.id ?? 0));
+            const response = await fetch(`${API_BASE_URL}/api/board/delete`, {
+              method: "POST",
+              headers: {
+                Authorization: "",
+              },
+            });
+            const result = await response.json(); // 서버 응답을 JSON으로 파싱
+            if (!result?.success) {
+              alert(`삭제 실패. ${result?.msg}`);
+              return;
+            }
+            navigate("/");
+          } catch (error: any) {
+            console.log(`서버 에러! ${error?.message ?? ""}`);
+          }
+        }}
+      >
+        삭제
+      </button>
+
+      <button
+        onClick={() => {
+          navigate(`/memo`);
+        }}
+      >
+        리스트로 이동
+      </button>
+    </div>
+  );
+}
