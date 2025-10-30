0dca92c8a14a648f5a240a0ee44e8752f584620d
najongjine <najongjin3@hotmail.com>
2025-10-30 11:33:47 +0900

    .

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as gtypes from "../types/global_types";
+import * as utils from "../utils/utils";
+import { useShallow } from "zustand/shallow";
+import { useAuthStore } from "../store/authStore";
 import "./Home.css";
+import { auth } from "../utils/firebaseConfig";
 
 export default function Home() {
+  // 이 방법은 상태가 바뀔 때만 리렌더링됩니다.
+  const userInfo = useAuthStore((state) => state.userInfo);
+
+  // 2. 액션 함수 가져오기 (SET을 위한 함수)
+  const { login, logout } = useAuthStore(
+    useShallow((state) => ({
+      login: state.login,
+      logout: state.logout,
+    }))
+  );
   const navigate = useNavigate();
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
   const [memo, setMemo] = useState<gtypes.Memotype[]>([]);
 
   useEffect(() => {
+    validation();
     getMemoList();
   }, []);
+
+  async function validation() {
+    let result = await utils.verify_token(userInfo?.token ?? "");
+    if (result.includes("인증실패")) {
+      await auth.signOut();
+      logout();
+    }
+  }
 
   function onMemoDetail(id = 0) {
     navigate(`/memo_detail?id=${id}`);
