ae0c44141d0088fafeff4ed6246d5196cfab8ed4
najongjine <najongjin3@hotmail.com>
2025-10-30 11:15:32 +0900

    .

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   // 1. 상태 읽기 (READ)
   // useAuthStore 훅을 통해 현재 상태에서 userInfo를 가져옵니다.
   // 이 방법은 상태가 바뀔 때만 리렌더링됩니다.
-  const userInfo = useAuthStore((state) => state.userInfo);
+  const userInfo = useAuthStore((state) => state?.userInfo);
 
   // 2. 액션 함수 가져오기 (SET을 위한 함수)
   const { login, logout } = useAuthStore(
