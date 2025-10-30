7879663f7191f14bd8fdd1669a0a7d8612774252
najongjine <najongjin3@hotmail.com>
2025-10-30 10:17:04 +0900

    .

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"; // useState 추가
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
 import { useShallow } from "zustand/shallow";
 
@@ -17,40 +17,31 @@ export default function Header() {
       logout: state.logout,
     }))
   );
-  // UI 구현을 위한 임시 상태. 실제 기능 구현 시 변경될 수 있습니다.
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태
-  const [userName, setUserName] = useState("a.com"); // 유저 이름
-
-  // 버튼 클릭 핸들러 (UI 테스트용)
-  const handleAuthClick = () => {
-    // 실제 로그인/로그아웃 로직이 들어갈 곳
-    if (isLoggedIn) {
-      console.log("로그아웃 시도");
-      setIsLoggedIn(false);
-      setUserName(""); // 로그아웃 시 이름 초기화
-    } else {
-      console.log("로그인 시도");
-      setIsLoggedIn(true);
-      setUserName("a.com"); // 로그인 성공 시 이름 설정
-    }
-  };
+  const navigate = useNavigate();
 
   return (
     <div>
       <header>
         <h1>헤더에요</h1>
         <div className="header-right">
-          {isLoggedIn ? (
+          {userInfo?.id ? (
             // 로그인 상태일 때
             <>
-              <span className="user-info">{userName}님 환영합니다</span>
-              <button className="auth-button" onClick={handleAuthClick}>
+              <span className="user-info">
+                {userInfo?.displayName ?? ""}님 환영합니다
+              </span>
+              <button className="auth-button" onClick={() => {}}>
                 로그아웃
               </button>
             </>
           ) : (
             // 로그아웃 상태일 때
-            <button className="auth-button" onClick={handleAuthClick}>
+            <button
+              className="auth-button"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
               로그인
             </button>
           )}
