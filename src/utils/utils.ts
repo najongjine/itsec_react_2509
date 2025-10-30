923c2b345b11b714e9f0ee4f0b6c676b69a2c38e
najongjine <najongjin3@hotmail.com>
2025-10-30 11:39:25 +0900

    .

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,6 +3,7 @@
 
 import { useShallow } from "zustand/shallow";
 import { useAuthStore } from "../store/authStore";
+import { auth } from "./firebaseConfig";
 
 // 이 방법은 상태가 바뀔 때만 리렌더링됩니다.
 const userInfo = useAuthStore((state) => state.userInfo);
@@ -16,6 +17,14 @@ const { login, logout } = useAuthStore(
 );
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+/**
+ * 토큰 검증 함수.
+ * 서버에서 문제있을시 alert 띄움.
+ * 리엑트에서 문법에러 있을시 alert 띄움.
+ * 토큰만료나 위조시 로그아웃 처리하고 alert 띄움.
+ * 토큰이 정상이면 아무것도 안함.
+ * return 값들은 혹시 몰라서 그냥 만들어 놓음.
+ */
 export async function verify_token(token: string) {
   try {
     const response = await fetch(`${API_BASE_URL}/api/user/verify_token`, {
@@ -30,6 +39,9 @@ export async function verify_token(token: string) {
     if (result?.data) {
       return "인증성공";
     }
+    await auth.signOut();
+    logout();
+    alert(`로그인 기간이 만료 되었습니다. \n 로그인을 다시 해주세요.`);
     return "인증실패";
   } catch (error: any) {
     alert(`에러. ${error?.message ?? ""}`);
