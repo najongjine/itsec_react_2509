6fea74488d307ec560aa18e16e839670377febd1
najongjine <najongjin3@hotmail.com>
2025-11-03 09:16:54 +0900

    .

---
+++ src/page/ImgTest.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export default function ImgTest() {
+  useEffect(() => {}, []);
+
+  return (
+    <div className="content-margin-padding">
+      <div>img test</div>
+    </div>
+  );
+}
